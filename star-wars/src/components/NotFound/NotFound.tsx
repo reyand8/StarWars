b6f1937d34a48b0730d6559c0ae2430205f4f097
Andrey <andreeey.gg@gmail.com>
2024-10-20 14:19:42 +0200

    add NotFound

---
+++ star-wars/src/components/NotFound/NotFound.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Box, Typography } from '@mui/material';
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import ErrorIcon from '@mui/icons-material/Error';
+import { Theme } from '@mui/material/styles/createTheme';
+
+
+const PaperError = styled(Paper)(({ theme }: { theme: Theme }) => ({
+    maxWidth: '800px',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    margin: 'auto',
+    backgroundColor: theme.palette.secondary.main
+}));
+
+const ErrorText = styled(Typography)(({ theme }: { theme: Theme }) => ({
+    fontWeight: 'bold',
+    margin: '12px 0',
+    textAlign: 'center',
+}));
+
+const StyledErrorIcon = styled(ErrorIcon)(({ theme }: { theme: Theme }) => ({
+    fontSize: 60,
+    marginTop: '18px',
+    fill: theme.palette.error.main,
+}));
+
+const NotFound: React.FC = () => {
+    return (
+        <Box sx={{marginTop: 2}}>
+            <PaperError>
+                <StyledErrorIcon />
+                <ErrorText>Not Found</ErrorText>
+            </PaperError>
+        </Box>
+    );
+};
+
+export default NotFound;
--- star-wars/src/routes/AppRoutes.tsx
+++ star-wars/src/routes/AppRoutes.tsx
@@ -3,11 +3,13 @@ import {Route, Routes} from 'react-router-dom';
 
 import HeroList from "../components/HeroList/HeroList";
 import HeroDetails from "../components/HeroDetails/HeroDetails";
+import NotFound from "../components/NotFound/NotFound";
 
 const AppRoutes: React.FC = () => (
     <Routes>
         <Route index element={<HeroList />} />
         <Route path='/hero/:id' element={<HeroDetails />} />
+        <Route path='*' element={<NotFound />} />
     </Routes>
 );
 
