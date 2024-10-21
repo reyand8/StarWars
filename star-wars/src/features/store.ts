7f1ee1c5de5aac504f1aa58a00384e657cd3e1ca
Andrey <andreeey.gg@gmail.com>
2024-10-21 17:04:26 +0200

    add new reducer

--- star-wars/src/features/store.ts
+++ star-wars/src/features/store.ts
@@ -2,19 +2,25 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import heroesReducer from './allHeroesSlice/allHeroesSlice';
 import heroDetailsReducer from './heroDetailsSlice/heroDetailsSlice';
+import { searchHeroSlice } from './searchHeroSlice/searchHeroSlice';
 
 /**
 * Creates and configures the global Redux store.
 *
 * @property {object} reducer - This object contains the reducers that manage different slices of the app's state.
-* @property {function} heroesReducer - This reducer responsible for managing the state of all heroes.
-* @property {function} heroDetailsReducer - This reducer responsible for managing the state of the selected hero's details.
+* @property {function} allHeroes - This reducer is responsible for managing the state of all heroes.
+* @property {function} heroDetails - This reducer is responsible for managing the state of the selected hero's details.
+* @property {function} searchHeroSlice - This reducer is responsible for managing the state associated with searching for heroes.
+* @property {function} middleware - This function is used to apply middleware to the Redux store.
 */
 const store = configureStore({
     reducer: {
         allHeroes: heroesReducer,
         heroDetails: heroDetailsReducer,
+        [searchHeroSlice.reducerPath]: searchHeroSlice.reducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+        .concat(searchHeroSlice.middleware),
 });
 
 /**
